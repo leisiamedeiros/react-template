72aad7f1466d3867dac5340dc4a57464899e2afd
leisiamedeiros <leisiamedeiros@gmail.com>
2021-05-09 21:08:14 -0300

    my app name

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -32,7 +32,7 @@ export default function Layout(props: any) {
 export function Header() {
     return (
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-            <Link to="/" className="navbar-brand col-md-3 col-lg-2 me-0 px-3">Meu App</Link>
+            <Link to="/" className="navbar-brand col-md-3 col-lg-2 me-0 px-3">My App</Link>
 
             <ul className="navbar-nav px-3">
                 <li className="nav-item text-nowrap">
